f463c8a8fa2039b148d3df06f8addc85a54dfc8c
Hang Phan <hangphan0508@gmail.com>
2025-10-21 13:58:30 +0200

    Person B changes the println

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
-public class Main {
-	public static void main(Strings[]args){
-		System.out.println("Hello,World!");
 public class Main { 
     public static void main(String[] args) { 
           System.out.println("Hello from Person B!");   // Ändring av Person B 
     } 
-}
-
-
 }
