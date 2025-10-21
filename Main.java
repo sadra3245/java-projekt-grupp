d985a36c590149ac0c22ba541d55ebbd5dff0094
Sadra Javad <sadramohtork@gmail.com>
2025-10-21 14:07:37 +0200

    Person A rules

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
 	public static void main(Strings[]args){
-	 System.out.println("Hello from Person A and Person B!");
+	 System.out.println("Hello from  only Person A and  not Person B!");
 }
 }
